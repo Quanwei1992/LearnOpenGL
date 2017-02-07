8e51e26d3732162681d218d7521e76a3a56e6ce6
袁全伟 <573011406@qq.com>
2017-02-07 15:42:27 +0800

    完成1-8 变换

---
+++ LearnOpenGL/8-Transformations/Shader.cpp
@@ -0,0 +1,85 @@
+#include "Shader.h"
+
+
+
+Shader::Shader(const GLchar * vertexPath, const GLchar * fragmentPath)
+{
+	// 1.从文件路径中获取顶点/片段着色器
+	std::string vertexCode;
+	std::string fragmentCode;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
+
+    // 2.保证ifrtream对象可以抛出异常
+	vShaderFile.exceptions(std::ifstream::badbit);
+	vShaderFile.exceptions(std::ifstream::badbit);
+	try
+	{
+		// 打开文件
+		vShaderFile.open(vertexPath);
+		fShaderFile.open(fragmentPath);
+		std::stringstream vShaderStream, fShaderStream;
+		vShaderStream << vShaderFile.rdbuf();
+		fShaderStream << fShaderFile.rdbuf();
+
+		vShaderFile.close();
+		fShaderFile.close();
+		
+		vertexCode = vShaderStream.str();
+		fragmentCode = fShaderStream.str();
+	}
+	catch (std::ifstream::failure e)
+	{
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+	}
+
+	const GLchar* vShaderCode = vertexCode.c_str();
+	const GLchar* fShaderCode = fragmentCode.c_str();
+
+	GLuint vertex, fragment;
+	GLint sucess;
+	GLchar infoLog[512];
+
+	// 顶点着色器
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vShaderCode, NULL);
+	glCompileShader(vertex);
+
+	glGetShaderiv(vertex, GL_COMPILE_STATUS, &sucess);
+	if (!sucess) {
+		glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// 片段着色器
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fShaderCode, NULL);
+	glCompileShader(fragment);
+
+	glGetShaderiv(fragment, GL_COMPILE_STATUS, &sucess);
+	if (!sucess) {
+		glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::fragment::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertex);
+	glAttachShader(shaderProgram, fragment);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &sucess);
+	if (!sucess) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::ShaderProgram::LINK_FAILED\n" << infoLog << std::endl;
+	}
+
+	this->Program = shaderProgram;
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+}
+
+void Shader::Use()
+{
+	glUseProgram(this->Program);
+}
